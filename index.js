7717c809cbde87870da89551f0e1ab6cdb3a57ab
Sakari Hakala <sakari.hakala@solita.fi>
2019-05-05 21:28:01 +0300

    osa3.2 puhelinluettelo be step2.

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/info', (req, res) => {
+    const count = persons.length
+    const date = new Date()
+
+    res.send(`<p>Puhelinluettelossa on ${count} henkilön tiedot</p><p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server is runninng on port ${PORT}`)
